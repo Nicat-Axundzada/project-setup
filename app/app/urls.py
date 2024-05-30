e0f9994f735bdb6f4232a79833949f9d983a18f0
Nicat-Axundzada <nicataxundzada@gmail.com>
2024-05-30 15:22:33 +0400

    Adding configuration for static and media files

--- app/app/urls.py
+++ app/app/urls.py
@@ -21,6 +21,8 @@ from drf_spectacular.views import (
 
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,3 +30,7 @@ urlpatterns = [
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='api-schema'),
          name='api-docs'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
